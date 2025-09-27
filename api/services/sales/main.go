d6767782899ef6115cf6afd61290bfc7e8f01b96
Nas Issa <nasissa97@gmail.com>
2025-09-26 18:03:29 -0700

    Commit 5

--- api/services/sales/main.go
+++ api/services/sales/main.go
@@ -10,6 +10,8 @@ import (
 	"github.com/nasissa97/service/foundation/logger"
 )
 
+var build = "develop"
+
 func main() {
 	var log *logger.Logger
 	events := logger.Events{
@@ -34,7 +36,7 @@ func main() {
 
 func run(ctx context.Context, log *logger.Logger) error {
 
-	log.Info(ctx, "startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+	log.Info(ctx, "startup", "GOMAXPROCS", runtime.GOMAXPROCS(0), build)
 
 	shutdown := make(chan os.Signal, 1)
 	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
