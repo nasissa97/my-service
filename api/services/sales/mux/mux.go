b174fb7dbfc6c8029833b2f4df6760ca4985ac73
Nas Issa <nasissa97@gmail.com>
2025-10-03 17:54:12 -0700

    Commit 16

--- api/services/sales/mux/mux.go
+++ api/services/sales/mux/mux.go
@@ -6,13 +6,14 @@ import (
 	"os"
 
 	"github.com/nasissa97/service/api/services/sales/route/sys/checkapi"
+	"github.com/nasissa97/service/app/api/mid"
+	"github.com/nasissa97/service/foundation/logger"
 	"github.com/nasissa97/service/foundation/web"
 )
 
 // WebAPI constructs a http.Handler with all application routes bound.
-func WebAPI(shutdown chan os.Signal) *web.App {
-	// var opts Options
-	mux := web.NewApp(shutdown)
+func WebAPI(log *logger.Logger, shutdown chan os.Signal) *web.App {
+	mux := web.NewApp(shutdown, mid.Logger(log))
 
 	checkapi.Routes(mux)
 
